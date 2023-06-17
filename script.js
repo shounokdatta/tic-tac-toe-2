7eb53ad03ab3e94f19d8ee715b28b5bd7740f941
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2023-06-17 10:21:38 +0530

    chenges

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let music=new Audio("item/music.mp3");
 let audioTurn=new Audio("item/ting.mp3");
 let gameOver=new Audio("item/gameover.mp3");
+let img=document.getElementById("img");
+let btn=document.getElementById("btn");
 let turn="x"; 
 let isgameover=false;
 
 //function to change the turn
 const changeTurn = ()=>{
     return turn ==="x"?"0":"x"
-    
 }
-
 //function to check for a win 
 function checkWin()
-{
-        
-        
+{   
     let wins=[
              [0, 1, 2],
              [3, 4, 5],
@@ -25,17 +23,15 @@ function checkWin()
              [0, 4, 8],
              [2, 4, 6],
             ];
-           
-        
             wins.forEach(e =>{
             if((boxes[e[0]].innerText === boxes[e[1]].innerText) && (boxes[e[2]].innerText === boxes[e[1]].innerText) && (boxes[e[0]].innerText!=="" ))
             {
-              console.log("it working");
               document.querySelector(".info").innerText=boxes[e[0]].innerText+" won";
               isgameover=true;
+              gameOver.play();
+              img.style.width="200px";
              }
-         })   
-
+         })     
     }
 
 //game logic
@@ -47,10 +43,21 @@ Array.from(boxes).forEach(element=>{
         {
             element.innerHTML=turn;
             turn=changeTurn();
-            audioTurn.play();
             checkWin();
             if(!isgameover){
+                audioTurn.play();
                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn; }    
         }
     })
 })
+// add button logic
+btn.addEventListener("click", ()=>{
+    let boxtexts = document.querySelectorAll('.box');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = "";
+    });
+    turn = "x"; 
+    isgameover = false;
+    img.style.width="0px";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;     
+});
